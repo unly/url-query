178ff721d1f04a7778afa9490defb9a3c7829b90
unly <leon.graser@deliveryhero.com>
2026-03-03 15:32:29 +0100

    fix: if omitempty is empty without a field name default to lowercase

--- query/tags.go
+++ query/tags.go
@@ -14,12 +14,21 @@ const (
 func getNameTags(field *reflect.StructField) []string {
 	value, ok := field.Tag.Lookup(TagName)
 	if !ok {
-		fieldName := []rune(field.Name)
-		fieldName[0] = unicode.ToLower(fieldName[0])
-		return []string{string(fieldName)}
+		return []string{defaultName(field)}
 	}
 
-	return strings.Split(value, ",")
+	parts := strings.Split(value, ",")
+	if parts[0] == "" {
+		parts[0] = defaultName(field)
+	}
+
+	return parts
+}
+
+func defaultName(field *reflect.StructField) string {
+	fieldName := []rune(field.Name)
+	fieldName[0] = unicode.ToLower(fieldName[0])
+	return string(fieldName)
 }
 
 func getDefaultTags(field *reflect.StructField) []string {
